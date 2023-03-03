a6d37a11c104cfaf1b1a0dbc962352bc9d356f1c
Elaine <e.cristina00@gmail.com>
2023-03-02 21:41:02 -0300

    Criado o projeto e a classe vetor.

---
+++ estruturadados-algoritmos-java/src/com/elaine/estruturadados/vetor/Vetor.java
@@ -0,0 +1,9 @@
+package com.elaine.estruturadados.vetor;
+
+public class Vetor {
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+}
